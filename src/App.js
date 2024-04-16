cee15074adc662d695a0919b5983037c34c6d2eb
Daniel Navarrete Cuevas <danielnavarrete@Daniels-MacBook-Pro.local>
2024-04-15 22:36:15 -0700

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
+import React, { useEffect } from 'react';
+
+
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Daniel Navarrete";
+  }, []); // This useEffect hook will run once when the component mounts
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+    <Hero/>
+    <Skills/>
+    <About/>
+    <Work/>
+    <Contact/>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,141 @@
+import React from 'react'
+import movies from '../assets/MovieWebsite.png'
+import agency from '../assets/MexicoWebsite.png'
+import shop from '../assets/ecommerce.png'
+
+const Work = () => {
+  return (
+    <div className='max-w-[1200px] mx-auto p-5' id='work'>
+        <div className='pb-8'>
+            <p className='text-4xl mb-3 font-bold primary-color'>Projects</p>
+            <p className='text-gray-400'>Check my projects where I show my skills in different areas.</p>
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+                            shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center
+                            h-[200px] bg-cover relative'>
+                    <img src={movies} alt="movies project" />
+                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col
+                                    justify-center items-center'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>Movies</span>
+                            <div className='pt-8 text-center'>
+                               <a href="https://danielncuevas10.github.io/cinemaworld/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
+                                                    font-bold text-lg'>
+                                                        Live
+                                    </button>
+                                </a>
+                            </div>
+
+                    </div>
+
+            </div>
+
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+                            shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center
+                            h-[200px] bg-cover relative'>
+                    <img src={agency} alt="movies project" />
+                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col
+                                    justify-center items-center'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>Movies</span>
+                            <div className='pt-8 text-center'>
+                               <a href="https://danielncuevas10.github.io/cinemaworld/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
+                                                    font-bold text-lg'>
+                                                        Live
+                                    </button>
+                                </a>
+                            </div>
+
+                    </div>
+
+            </div>
+
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+                            shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center
+                            h-[200px] bg-cover relative'>
+                    <img src={shop} alt="movies project" />
+                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col
+                                    justify-center items-center'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>Movies</span>
+                            <div className='pt-8 text-center'>
+                               <a href="https://danielncuevas10.github.io/cinemaworld/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
+                                                    font-bold text-lg'>
+                                                        Live
+                                    </button>
+                                </a>
+                            </div>
+
+                    </div>
+
+            </div>
+
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+                            shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center
+                            h-[200px] bg-cover relative'>
+                    <img src={movies} alt="movies project" />
+                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col
+                                    justify-center items-center'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>Movies</span>
+                            <div className='pt-8 text-center'>
+                               <a href="https://danielncuevas10.github.io/cinemaworld/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
+                                                    font-bold text-lg'>
+                                                        Live
+                                    </button>
+                                </a>
+                            </div>
+
+                    </div>
+
+            </div>
+
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+                            shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center
+                            h-[200px] bg-cover relative'>
+                    <img src={movies} alt="movies project" />
+                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col
+                                    justify-center items-center'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>Movies</span>
+                            <div className='pt-8 text-center'>
+                               <a href="https://danielncuevas10.github.io/cinemaworld/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
+                                                    font-bold text-lg'>
+                                                        Live
+                                    </button>
+                                </a>
+                            </div>
+
+                    </div>
+
+            </div>
+
+            <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden
+                            shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center
+                            h-[200px] bg-cover relative'>
+                    <img src={movies} alt="movies project" />
+                    <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col
+                                    justify-center items-center'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>Movies</span>
+                            <div className='pt-8 text-center'>
+                               <a href="https://danielncuevas10.github.io/cinemaworld/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700
+                                                    font-bold text-lg'>
+                                                        Live
+                                    </button>
+                                </a>
+                            </div>
+
+                    </div>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Work
